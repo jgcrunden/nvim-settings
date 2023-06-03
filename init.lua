f21c6275015cc2eab482b89d875ca1f79e7d4933
Joshua Crunden <joshuacrunden@gmail.com>
2023-06-03 21:37:30 +0100

    Add relative number, ad two space formatting for css

--- init.lua
+++ init.lua
@@ -4,15 +4,11 @@ vim.opt.sts = 4
 vim.opt.sw = 4
 
 vim.g.mapleader = " "
+vim.wo.relativenumber = true
 
 vim.api.nvim_create_autocmd("FileType", {
 	command = "setlocal ts=2 sts=2 sw=2 expandtab",
-	pattern = { "yaml", "markdown", "xml" },
-})
-
-vim.api.nvim_create_autocmd("TermOpen", {
-	command = "setlocal nonumber",
-	pattern = "*",
+	pattern = { "yaml", "markdown", "xml", "css" },
 })
 
 vim.api.nvim_set_keymap("t", "<Esc>", '<C-\\><C-n>', { noremap = true }) -- Set <Esc> as shortcut to exit insert mode in terminal
