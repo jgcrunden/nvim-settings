6468e673a2d2a80eee63c4918cee8d62e77f21c3
Joshua Crunden <33781396+jgcrunden@users.noreply.github.com>
2024-08-29 15:57:13 +0000

    fix: replace append with add in harpoon, update trouble apis

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -2,8 +2,8 @@ require('trouble').setup()
 
 -- Lua
 vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>gL", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>gl", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>gL", "<cmd>Trouble diagnostics toggle<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", {silent = true, noremap = true})
